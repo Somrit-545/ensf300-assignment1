753e5d2824b82fef8833806beb850f89ffa6fb47
somrit <somritdewanstudent@gmail.com>
2025-09-29 13:48:54 -0600

    made changes to get_platform()

--- design_project.py
+++ design_project.py
@@ -12,11 +12,12 @@ import general_information as gi
 import user_csv as cs
 import time
 
-def error_massage():
+def error_message():
     '''This function prints a message when the user enters an invalid input'''
 
     print(f"-"*70)                                          # Prints "-" 70 times
-    print(f"Error!! Invalid Input, Please Try Again.\n")    # Prints the error statement
+    print(f"There's an error in your input. Please try again.")    # Prints the error statement
+    print(f"-"*70)
 
 def exit_message():
     '''This functions prints the exit message'''
@@ -43,30 +44,30 @@ def get_platform():
 
     # Checking for invalid input or "1"
     if (user_input != "0" and user_input != "1" and user_input not in gi.PLATFORM):
-        error_massage()         # Calls error message function
+        error_message()         # Calls error message function
         return get_platform()   # Recalls this function until valid input 
 
     elif (user_input == "1"):   # Checking if the user wants a list of the platforms
         print()
-
-        # Loops in platform list and prints with correct formating
-        for item_pos,item in enumerate(gi.PLATFORM):
-            if(item_pos == (len(gi.PLATFORM)-1)):
-                print(f"{item}.\n")         # Prints last item and prints new line
-            else:
-                print(f"{item}, ", end="")  # Prints item in the list
-        
+        platform_list()         # Calls the function to get the list of platforms
         return get_platform()   # Recalls this function
 
     else:
         # Checks if the user_input is a special case
         if user_input in gi.CHANGED.keys():
-            user_input = gi.CHANGED[user_input]     # Changes the input into an acceptable format
+            user_input = gi.CHANGED[user_input]     # Changes the special input into its popular name
             return user_input                       # Returns the new user_input
         else:
-            return user_input                       # Returns the original user_input unchanged
-   
+            return user_input                       # Returns the original user_input unchanged   
 
+def platform_list():
+    '''Prints out a list of gaming platforms'''
+
+    for item_pos,item in enumerate(gi.PLATFORM):
+        if(item_pos == (len(gi.PLATFORM)-1)):
+            print(f"{item}.\n")         # Prints last item and prints new line
+        else:
+            print(f"{item}, ", end="")  # Prints item in the list
 
 
 def menu_option():
@@ -88,15 +89,84 @@ def menu_option():
 
     # Checking for invalid inputs
     if (user_input != "0" and user_input != "1" and user_input != "2" and user_input != "3"):
-        error_massage()         # Calls error message function
+        error_message()         # Calls error message function
         return menu_option()    # Recalls this function until a correct valid input
     else:
         return user_input       # Returns the user selection as a string
 
+def get_max(data):
+    '''Uses all relevant information to find max rank, sale numbers and most popular genre
+    
+    Parameters:
+        data (lst): Contains all relevant information to calculate max
+            [all_ranks,na_sales,eu_sales,jp_sales,other_sales,global_sales, all_genre]   
+    
+    Returns:
+        list: [highest rank, Most sales (na),Most sales (eu) ,Most sales (jp), Most sales (other), Most sales (globally), how many times each genre appeared]
+    '''
+
+    # declare all nessesary variables
+    initial_genre_list = [0,0,0,0,0,0,0,0,0,0,0,0]
+    
+    # converting list into numpy arrays
+    rank_array = np.array(data[0])
+    na_sales_array = np.array(data[1])
+    eu_sales_array = np.array(data[2])
+    jp_sales_array = np.array(data[3])
+    other_sales_array = np.array(data[4])
+    global_sales_array = np.array(data[5])
+
+    # loops to count how many times each genre is present
+    for j in data[6]:
+        for k in range(len(gi.ALL_GENRE)):
+            if j == gi.ALL_GENRE[k]:
+                initial_genre_list[k] += 1
+    
+    return [rank_array.min(), na_sales_array.max(),eu_sales_array.max(),jp_sales_array.max(),other_sales_array.max(), global_sales_array.max(), initial_genre_list]
+
+def print_max_values(plateform,info,rank_list,sale_list):
+    '''Find the rest of the nessesary information and extra details to print the information to the user in a nice format
+    
+    Parameters:
+        plateform (str): a valid plateform
+        info (str): a list containing [highest rank, Most sales (na),Most sales (eu) ,Most sales (jp), Most sales (other), Most sales (globally), how many times each genre appeared]
+        rank (nested lst): a list containing multiple list containing [Rank,Name,Platform] of all games in data
+        sale (nested lst): a list containing multiple list containing [Platform,NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales] of all games in data
+        genre (nested lst): a list containing multiple list containing [Platform,Year,Genre,Publisher] of all games in data
+    
+    ''' 
+
+    # Declaring nessesary variables
+    num_which_info_is_found_at = [0,0,0,0,0,0,0]        # lst which holds the index at which valuable information is found at
+    value_of_max_genre = 0                              # how many games are in the most popular genre
+    index_where_max_genre_is_at = 0                     # The index which indicates which genre is the most popular
+    
+
+    # Loop to find index where important information is found
+    for i in range (len(rank_list)):                # checking orignal data list
+        if rank_list[i][0] == info[0]:
+            num_which_info_is_found_at[0] = i       # changes the first integer of the list to the index where the information is found 
+        
+
+        for j in range(1,6):                        # checks all sales, (NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales)
+            if float(sale_list[i][j]) == info[j]:
+                num_which_info_is_found_at[j] = i   # changes a specific index of the list to the index at which the information is found 
+
+        # Find the most popular genre
+        for pos,val in enumerate(info[6]):
+            if val > value_of_max_genre:
+                value_of_max_genre = val            # assigns the number of games of the most popular genre
+                index_where_max_genre_is_at = pos   # assigns the position, which can be used to identify the genre
 
 
-
-
+    # Prints all the data for the user
+    print(f"\nMaximum Data:")
+    print(f"The highest ranking video game in terms of sales is {rank_list[num_which_info_is_found_at[0]][1]} ranking {info[0]} out of 16600")
+    # loop to automatically print all the sale number values
+    for k in range(1,6):
+        print(f"The highest selling video game {gi.REGION_PRINT[k-1]} for the {plateform} is '{rank_list[num_which_info_is_found_at[k]][1]}' selling {info[k]} million units.")
+    print(f"The most popular genre is {gi.ALL_GENRE[index_where_max_genre_is_at]} having {value_of_max_genre} games.")
+    print()
 
 def aquiring_data_to_useable_form(plateform,rank,sale,genre):
     '''Pulls the correct information according to user selection of the platform
@@ -146,40 +216,51 @@ def aquiring_data_to_useable_form(plateform,rank,sale,genre):
     return [all_ranks,na_sales,eu_sales,jp_sales,other_sales,global_sales, all_genre]
 
 
-
-
-
-def get_max(data):
-    '''Uses all relevant information to find max rank, sale numbers and most popular genre
+def print_max_values(platform,info,rank_list,sale_list):
+    '''Finds the nessesary information to print max values and statistics to the user in plain English
     
     Parameters:
-        data (lst): Contains all relevant information to calculate max
-            [all_ranks,na_sales,eu_sales,jp_sales,other_sales,global_sales, all_genre]   
+        platform (str): a valid platform
+        info (str): a list containing [highest rank, Most sales (na),Most sales (eu) ,Most sales (jp), Most sales (other), Most sales (globally), how many times each genre appeared]
+        rank (nested lst): a list containing multiple list containing [Rank,Name,Platform] of all games in data
+        sale (nested lst): a list containing multiple list containing [Platform,NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales] of all games in data
     
-    Returns:
-        list: [highest rank, Most sales (na),Most sales (eu) ,Most sales (jp), Most sales (other), Most sales (globally), how many times each genre appeared]
-    '''
+    ''' 
 
-    # declare all nessesary variables
-    initial_genre_list = [0,0,0,0,0,0,0,0,0,0,0,0]
+    # Declaring nessesary variables
+    num_which_info_is_found_at = [0,0,0,0,0,0,0]        # lst which holds the index at which valuable information is found at
+    value_of_max_genre = 0                              # how many games are in the most popular genre
+    index_where_max_genre_is_at = 0                     # The index which indicates which genre is the most popular
     
-    # converting list into numpy arrays
-    rank_array = np.array(data[0])
-    na_sales_array = np.array(data[1])
-    eu_sales_array = np.array(data[2])
-    jp_sales_array = np.array(data[3])
-    other_sales_array = np.array(data[4])
-    global_sales_array = np.array(data[5])
 
-    # loops to count how many times each genre is present
-    for j in data[6]:
-        for k in range(len(gi.ALL_GENRE)):
-            if j == gi.ALL_GENRE[k]:
-                initial_genre_list[k] += 1
-    
-    return [rank_array.min(), na_sales_array.max(),eu_sales_array.max(),jp_sales_array.max(),other_sales_array.max(), global_sales_array.max(), initial_genre_list]
+    # Loop to find index where important information is found
+    for i in range (len(rank_list)):                # checking orignal data list
+        if rank_list[i][0] == info[0]:
+            num_which_info_is_found_at[0] = i       # changes the first integer of the list to the index where the information is found 
+        
 
-def print_max_values(plateform,info,rank_list,sale_list,genre_list):
+        for j in range(1,6):                        # checks all sales, (NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales)
+            if float(sale_list[i][j]) == info[j]:
+                num_which_info_is_found_at[j] = i   # changes a specific index of the list to the index at which the information is found 
+
+        # Find the most popular genre
+        for pos,val in enumerate(info[6]):
+            if val > value_of_max_genre:
+                value_of_max_genre = val            # assigns the number of games of the most popular genre
+                index_where_max_genre_is_at = pos   # assigns the position, which can be used to identify the genre
+
+
+    # Prints all the data for the user
+    print(f"\nMaximum Data:")
+    print(f"The highest ranking video game in terms of sales is {rank_list[num_which_info_is_found_at[0]][1]} ranking {info[0]} out of 16600")
+    # loop to automatically print all the sale number values
+    for k in range(1,6):
+        print(f"The highest selling video game {gi.REGION_PRINT[k-1]} for the {platform} is '{rank_list[num_which_info_is_found_at[k]][1]}' selling {info[k]} million units.")
+    print(f"The most popular genre is {gi.ALL_GENRE[index_where_max_genre_is_at]} having {value_of_max_genre} games.")
+    print()
+
+
+def print_max_values(plateform,info,rank_list,sale_list):
     '''Find the rest of the nessesary information and extra details to print the information to the user in a nice format
     
     Parameters:
@@ -222,9 +303,6 @@ def print_max_values(plateform,info,rank_list,sale_list,genre_list):
         print(f"The highest selling video game {gi.REGION_PRINT[k-1]} for the {plateform} is '{rank_list[num_which_info_is_found_at[k]][1]}' selling {info[k]} million units.")
     print(f"The most popular genre is {gi.ALL_GENRE[index_where_max_genre_is_at]} having {value_of_max_genre} games.")
     print()
-
-
-
 
 
 
@@ -543,15 +621,15 @@ while True:
 
         
         if user_menu_option == "1":         # if the user chooses to find out about the max data
-            print_max_values(user_platform, get_max(aquiring_data_to_useable_form(user_platform,rank,sale_number, genre)),rank,sale_number, genre) 
+            print_max_values(user_platform, get_max(aquiring_data_to_useable_form(user_platform,rank,sale_number,genre)),rank,sale_number) 
             max_search += 1                 # adds another to the count of how many times the user searched this
 
         elif(user_menu_option == "2"):      # if the user chooses to find out about the min data
-            print_min_values(user_platform, get_min(aquiring_data_to_useable_form(user_platform,rank,sale_number, genre)),rank,sale_number, genre)
+            print_min_values(user_platform, get_min(aquiring_data_to_useable_form(user_platform,rank,sale_number, genre)),rank,sale_number)
             min_search += 1                 # adds another to the count of how many times the user searched this
 
         elif(user_menu_option == "3"):      # if the user chooses to find out about the average data
-            print_ave_values(user_platform, get_ave(aquiring_data_to_useable_form(user_platform,rank,sale_number, genre)),rank,sale_number, genre)
+            print_ave_values(user_platform, get_ave(aquiring_data_to_useable_form(user_platform,rank,sale_number, genre)),rank,sale_number)
             mean_search += 1                # adds another to the count of how many times the user searched this
 
         elif(user_menu_option == "0"):
