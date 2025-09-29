db849a644ac97ff75077283de2f328d10e3735b9
Somrit-545 <82519554+Somrit-545@users.noreply.github.com>
2025-09-29 13:16:20 -0600

    Add files via upload
    I added additional csv files and some python files which were necessary for the design_project.py to work.

---
+++ general_info.py
@@ -0,0 +1,6 @@
+# Contains all constants that depend of Data
+ALL_GENRE = ["Action", "Adventure", "Fighting", "Misc", "Puzzle", "Plateform", "Role-Playing", "Racing", "Shooter", "Simulation", "Sports", "Strategy"]
+PLATFORM = ["2600", "SAT", "3DO", "3DS", "DC", "DS", "GB","GBA","GC","GEN","GG","N64","NES","NG","PC","PCFX","PS","PS2","PS3","PS4","PSP","PSV","SCD","SNES","TG16","WII","WIIU","WS","X360","XB","XONE"]
+REGION = ["NA", "EU", "JP", "OTHER", "GLOBAL"]
+REGION_PRINT = ["in North America", "in Europe", "in Japan", "in other countries", "globally"]
+CHANGED = {"WII": "Wii", "WIIU": "WiiU", "XONE": "XOne", "2600": 2600}
